9d6d2c26e7b59d694e910aa1196ebefa73b12620
abubakar1014 <mianabubakar278@gmail.com>
2024-02-28 18:49:39 +0500

    use urls from db query

--- knx/models.py
+++ knx/models.py
@@ -14,10 +14,20 @@ class ProfileData(TimeStamped):
     location = models.CharField(max_length=500,  blank=True, null=True)
 
     class Meta:
-        unique_together = ['company_name', 'phone_number']
+        unique_together = ('company_name', 'phone_number')
     
     def __str__(self):
         return f"{self.company_name} {self.owner_name}"
     
 class ScraperDetail(TimeStamped):
-    count = models.CharField(max_length=20)
+    country_name = models.CharField(max_length=500,  blank=True, null=True, default="N/A")
+    count = models.PositiveIntegerField(null=True, blank=True, default=0)
+    url = models.CharField(max_length=500,  blank=True, null=True, default="N/A")
+    
+    class Meta:
+        unique_together = ('country_name', 'url')
+    
+    def __str__(self):
+        return f"{self.country_name} - {self.count}"
+    
+    
